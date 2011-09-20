8112a18a88cfe1bb9a3f53dd0584622ff40b1cd5
Bogdan <buhaibogdan@pentalog.fr>
2011-09-20 11:07:38 +0300

    My initial commit. Local?

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+echo "hello git";
+
+$vector = array(2,3,4,5);
+var_dump($vector);//do not commit this line
+//where did the for go? mhhhh
+foreach($vector as $elem)
+{
+	echo $elem, ', ';
+}
+
+echo '##########################';
